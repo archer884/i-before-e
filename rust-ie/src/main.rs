408e455e22178d37e45d4b5f8ee777bd714bd0d4
J/A <jarcher@balglobal.com>
2019-02-06 16:55:28 -0600

    Charges: attempted optimization

--- rust-ie/src/main.rs
+++ rust-ie/src/main.rs
@@ -26,25 +26,33 @@ fn main() -> io::Result<()> {
     let handle = io::stdout();
     let filter = Filter::new();
 
-    let mut buf = String::new();
     let mut writer = handle.lock();
-
-    for word in word_list(&mut buf)? {
+    visit_words(|word| {
         if !filter.is_valid(word) {
-            writeln!(writer, "{}", word)?;
+            write!(writer, "{}", word)?;
         }
+        Ok(())
+    })
+}
+
+// This goddamn mess is only very slightly faster, if at all, than the alternative in Master.
+fn visit_words(mut action: impl FnMut(&str) -> io::Result<()>) -> io::Result<()> {
+    use std::env;
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+
+    let path = env::args().nth(1).expect("provide file path");
+    let mut buf = String::new();
+    let mut input = File::open(path).map(BufReader::new)?;
+
+    while let Ok(len) = input.read_line(&mut buf) {
+        if len == 0 {
+            break;
+        }
+
+        action(&buf)?;
+        buf.clear();
     }
 
     Ok(())
-}
-
-fn word_list(buf: &mut String) -> io::Result<impl Iterator<Item = &str>> {
-    use std::env;
-    use std::fs::File;
-    use std::io::Read;
-
-    let _ = File::open(env::args().nth(1).expect("Provide filename"))
-        .and_then(|mut f| f.read_to_string(buf))?;
-
-    Ok(buf.split_ascii_whitespace())
 }
