e59c42a8a89c53fd4eff425bd740bd832edb1287
J/A <jarcher@balglobal.com>
2019-02-11 13:59:25 -0600

    Cleanup

--- rust-ie/src/main.rs
+++ rust-ie/src/main.rs
@@ -1,42 +1,13 @@
-use regex::Regex;
 use std::io::{self, Write};
-
-struct Filter {
-    // pattern: Regex,
-}
-
-impl Filter {
-    fn new() -> Self {
-        Self {
-            // pattern: Regex::new("([^c]|^)ei").unwrap(),
-        }
-    }
-
-    fn is_valid(&self, s: &str) -> bool {
-        let mut is_safe = false;
-        
-        for pair in s.as_bytes().windows(2) {
-            match pair {
-                [b'c', b'e'] => is_safe = true,
-                [b'e', b'i'] if !is_safe => return false,
-
-                _ => is_safe = false,
-            }
-        }
-
-        true
-    }
-}
 
 fn main() -> io::Result<()> {
     let handle = io::stdout();
-    let filter = Filter::new();
 
     let mut buf = String::new();
     let mut writer = handle.lock();
 
     for word in word_list(&mut buf)? {
-        if !filter.is_valid(word) {
+        if !is_valid(word) {
             writeln!(writer, "{}", word)?;
         }
     }
@@ -44,12 +15,27 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
+fn is_valid(s: &str) -> bool {
+    let mut is_safe = false;
+
+    for pair in s.as_bytes().windows(2) {
+        match pair {
+            [b'c', b'e'] => is_safe = true,
+            [b'e', b'i'] if !is_safe => return false,
+
+            _ => is_safe = false,
+        }
+    }
+
+    true
+}
+
 fn word_list(buf: &mut String) -> io::Result<impl Iterator<Item = &str>> {
     use std::env;
     use std::fs::File;
     use std::io::Read;
 
-    let _ = File::open(env::args().nth(1).expect("Provide filename"))
+    File::open(env::args().nth(1).expect("Provide filename"))
         .and_then(|mut f| f.read_to_string(buf))?;
 
     Ok(buf.split_ascii_whitespace())
